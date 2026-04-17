4d6d86ae9ee416e1221a2c24729af550d1e372c8
edquirino08 <eduardo.araujo.quirino@hotmail.com>
2026-04-17 14:32:30 -0300

    refactor: add user validation before create

--- src/main/java/com/taskflow/config/ApiResourceConfig.java
+++ src/main/java/com/taskflow/config/ApiResourceConfig.java
@@ -3,12 +3,14 @@ package com.taskflow.config;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.taskflow.common.exceptions.ValidationExceptionMapper;
 import com.taskflow.controller.user.UserController;
 
 public class ApiResourceConfig extends ResourceConfig {
     public ApiResourceConfig(JpaUnit jpaUnit) {
         register(JacksonFeature.class);
         register(UserController.class);
+        register(ValidationExceptionMapper.class);
         register(new DependencyBinder(jpaUnit));
     }
 }
